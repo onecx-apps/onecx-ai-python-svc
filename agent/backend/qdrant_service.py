66c978c95c36c537d2b76d1d5878753d553a69fc
dredinge <david.redinger@capgemini.com>
2023-12-20 12:40:21 +0100

    qdrant service changes

--- agent/backend/qdrant_service.py
+++ agent/backend/qdrant_service.py
@@ -4,29 +4,30 @@ from langchain.schema.embeddings import Embeddings
 from qdrant_client import QdrantClient, models
 from agent.utils.configuration import load_config
 from loguru import logger
-from omegaconf import DictConfig
 
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+QDRANT_URL=os.getenv("QDRANT_URL")
+QDRANT_PORT=os.getenv("QDRANT_PORT")
+QDRANT_API_KEY=os.getenv("QDRANT_API_KEY")
+QDRANT_COLLECTION_NAME=os.getenv("QDRANT_COLLECTION_NAME")
 
-@load_config(location="config/db.yml")
-def get_db_connection(cfg: DictConfig, embedding_model: Embeddings) -> Qdrant:
+
+def get_db_connection(embedding_model: Embeddings) -> Qdrant:
     """get_db_connection initializes the connection to the Qdrant db.
 
-    :param cfg: OmegaConf configuration
-    :type cfg: DictConfig
     :return: Qdrant DB connection
     :rtype: Qdrant
     """
     embedding = embedding_model
-    qdrant_client = QdrantClient(os.getenv("QDRANT_URL",cfg.qdrant.url), port=os.getenv("QDRANT_PORT",cfg.qdrant.port), api_key=os.getenv("QDRANT_API_KEY"), prefer_grpc=cfg.qdrant.prefer_grpc)
+    qdrant_client = QdrantClient(url=QDRANT_URL, port=QDRANT_PORT, api_key=QDRANT_API_KEY, prefer_grpc=False)
     try: 
-        qdrant_client.get_collection(collection_name=cfg.qdrant.collection_name)
+        qdrant_client.get_collection(QDRANT_COLLECTION_NAME)
     except Exception:
         qdrant_client.recreate_collection(
-            collection_name=cfg.qdrant.collection_name,
+            collection_name=QDRANT_COLLECTION_NAME,
             vectors_config=models.VectorParams(size=len(embedding.embed_query("Test text")), distance=models.Distance.COSINE),
         )
-        logger.info(f"SUCCESS: Collection {cfg.qdrant.collection_name} created.")
-    vector_db = Qdrant(client=qdrant_client, collection_name=cfg.qdrant.collection_name, embeddings=embedding)
+        logger.info(f'SUCCESS: Collection {QDRANT_COLLECTION_NAME} created.')
+    vector_db = Qdrant(client=qdrant_client, collection_name=QDRANT_COLLECTION_NAME, embeddings=embedding)
     logger.info("SUCCESS: Qdrant DB Connection.")
     return vector_db
