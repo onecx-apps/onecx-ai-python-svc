6d05f81f23686b7e07cf864b8d9dd8ab0fb7d90d
Michael Gloeckner <michael.gloeckner@capgemini.com>
2024-01-09 11:10:15 +0100

    fix for loading env in qdrant service

--- agent/backend/qdrant_service.py
+++ agent/backend/qdrant_service.py
@@ -2,8 +2,10 @@ import os
 from langchain.vectorstores import Qdrant
 from langchain.schema.embeddings import Embeddings
 from qdrant_client import QdrantClient, models
-from agent.utils.configuration import load_config
 from loguru import logger
+from dotenv import load_dotenv
+
+load_dotenv()
 
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
 QDRANT_URL=os.getenv("QDRANT_URL")
@@ -19,6 +21,7 @@ def get_db_connection(embedding_model: Embeddings) -> Qdrant:
     :rtype: Qdrant
     """
     embedding = embedding_model
+
     qdrant_client = QdrantClient(url=QDRANT_URL, port=QDRANT_PORT, api_key=QDRANT_API_KEY, prefer_grpc=False)
     try: 
         qdrant_client.get_collection(QDRANT_COLLECTION_NAME)
